8dc2a97ee26c1c422e5fe65046b4a8f1ad4f306c
Willem Vermeer <Willem.Vermeer@ing.com>
2020-10-28 22:13:03 +0100

    display content fetched thru grpc

--- webapp/src/main/scala/email/fizzle/zmail/web/WebappMain.scala
+++ webapp/src/main/scala/email/fizzle/zmail/web/WebappMain.scala
@@ -4,14 +4,16 @@ import email.fizzle.service.GetMailBoxRequest
 import email.fizzle.service.ZioService.MailBoxServiceClient
 import scalapb.grpc.Channels
 import scalapb.zio_grpc.ZManagedChannel
-import zio.App
-import zio.console.{ putStrLn, Console }
+import org.scalajs.dom
+import zio.{ App, ZIO }
+import zio.console.{ Console, putStrLn }
 
 object WebappMain extends App {
   val appLogic = for {
     _        <- putStrLn("Hello console 1")
     response <- MailBoxServiceClient.getMailBox(GetMailBoxRequest(username = "willem.vermeer"))
     _        <- putStrLn(response.toString)
+    _ <- ZIO.effectTotal(dom.document.getElementById("header").innerHTML = response.username)
     _        <- putStrLn("Hello console 2")
   } yield ()
 
